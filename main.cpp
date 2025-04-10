8b43b6c77a7d1587ba24c5b778c75289e476267c
kartoger <irisus.ivan@gmail.com>
2025-04-10 12:10:13 +0500

    Получаю user из строки и mac из файла

--- main.cpp
+++ main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
-int main() {
-      std::ifstream macFile("/sys/class/net/enp0s3/address");
-      if (!macFile.is_open()) {
-          std::cerr << "Не удалось открыть файл MAC-адреса!" << std::endl;
-          return 1;
-      }
-    char buffer[128];
-    std::string resultuser = "";
-    FILE* user = popen("whoami", "r");
-    while (fgets(buffer, sizeof(buffer), user) != nullptr) {
-        resultuser += buffer;
+std::string getMacAddress() {
+    std::ifstream macFile("/sys/class/net/enp0s3/address");
+    if (!macFile.is_open()) {
+        std::cerr << "Не удалось открыть файл MAC-адреса!" << std::endl;
+        return "unknown";
     }
-    pclose(user);
-
     std::string mac;
-      std::getline(macFile, mac);
-      macFile.close();
-      int Time = 0;
+    std::getline(macFile, mac);
+    macFile.close();
+    return mac;
+}
 
-      std::cout << "MAC: " << mac << ", Time: " << Time << ", User: " << resultuser <<  std::endl;
-      return 0;
-  }
+std::string getUser(std::string& line) {
+   std::regex re_invalid(R"(Failed password for (\w+))");
+    std::smatch match;
+        if (std::regex_search(line, match, re_invalid)){
+            return match[1];
+        } else {
+            return "unknown";
+        }
+
+}
+int main() {
+    std::string log="2025-04-10T11:48:42.523029+05:00 kartoger-VirtualBox sshd[9918]: Failed password for BAdsa_ds2 from 10.59.68.24 port 59840 ssh2";
+    std::string Mac = getMacAddress();
+    std::string user = getUser(log);
+    std::cout << "Mac: " << Mac << ", User: " << user << std::endl;
+}
+
