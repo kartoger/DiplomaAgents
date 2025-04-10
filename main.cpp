7d69098ebc93c797035141ffc941e96dd86700aa
kartoger <irisus.ivan@gmail.com>
2025-04-10 12:34:59 +0500

    Получаю время из строки лога

--- main.cpp
+++ main.cpp
@@ -15,20 +15,30 @@ std::string getMacAddress() {
     return mac;
 }
 
+std::string getTime(std::string& line) {
+    // Регулярка для времени (в начале строки)
+    std::regex timeRegex(R"(^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}))");
+    std::smatch timematch;
+    if (std::regex_search(line, timematch, timeRegex)) {
+        return timematch[1];
+    }
+    else {
+        return "unknown";
+    }
+}
 std::string getUser(std::string& line) {
    std::regex re_invalid(R"(Failed password for (\w+))");
-    std::smatch match;
-        if (std::regex_search(line, match, re_invalid)){
-            return match[1];
+    std::smatch usermatch;
+        if (std::regex_search(line, usermatch, re_invalid)){
+            return usermatch[1];
         } else {
             return "unknown";
         }
-
 }
 int main() {
     std::string log="2025-04-10T11:48:42.523029+05:00 kartoger-VirtualBox sshd[9918]: Failed password for BAdsa_ds2 from 10.59.68.24 port 59840 ssh2";
     std::string Mac = getMacAddress();
     std::string user = getUser(log);
-    std::cout << "Mac: " << Mac << ", User: " << user << std::endl;
+    std::string time = getTime(log);
+    std::cout <<"Time: " << time << ", Mac: " << Mac << ", User: " << user << std::endl;
 }
-
